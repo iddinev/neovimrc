ea185fc6afcd31a9713e705c1e393a035a792aee
Ivan Dinev <xidinev@gmail.com>
2026-07-21 16:01:24 +0300

    lualine extra configs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -124,11 +124,22 @@ require("lazy").setup({
             end,
           },
         },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
+      },
+      sections = {
+        lualine_c = {
+          {
+            "filename",
+            path = 1,
+          },
+        },
+      },
+      inactive_sections = {
+        lualine_c = {
+          {
+            "filename",
+            path = 1,
+          },
+        },
       },
     },
   },
